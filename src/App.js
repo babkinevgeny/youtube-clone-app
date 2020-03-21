b36baca9e58b949efa4582137f51c063d6dab6ce
babkinevgeny <babkinevgeny@gmail.com>
2020-03-21 23:40:59 +0300

    Add mechanism of receiving videos by searching value.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-
 import { SearchBar, Player, Info, Recomendations } from './components';
+import youtube from './api/youtube';
 
 class App extends Component {
+    state = {
+        videos: [],
+        currentVideo: null
+    }
+
+    submitHandler = async (searchValue) => {
+        const responce = await youtube.get('search', {
+            params: {
+                part: 'snippet',
+                q: searchValue,
+                maxResults: 5,
+                key: 'AIzaSyAYZcOWZNe_Xa9U991MR0NRsdefWmCItC8'
+            }
+        });
+        this.setState({
+            videos: responce.data.items,
+            currentVideo: responce.data.items[0]
+        });
+    }
+
     render() {
         return (
             <>
-                <SearchBar />
+                <SearchBar submitHandler={this.submitHandler}/>
                 <Player />
                 <Info />
                 <Recomendations />
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,34 @@
-import React from 'react'
+//import React from 'react';
+import React, { Component } from 'react';
+import { Paper, TextField } from '@material-ui/core';
 
-const SearchBar = () => {
-    return (
-        <div>
-            Search Bar
-        </div>
-    )
+export class SearchBar extends Component {
+    state = {
+        searchValue: ''
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        const { submitHandler } = this.props;
+        const { searchValue } = this.state;
+        submitHandler(searchValue);
+    }
+
+    searchHandler = (event) => {
+        this.setState({searchValue: event.target.value})
+    }
+
+    render() {
+        return (
+            <Paper>
+            <form onSubmit={(event) => this.submitHandler(event)}>
+                <TextField placeholder="Type what you want to look for" onChange={(event) => this.searchHandler(event)}/>
+            </form> 
+        </Paper>
+        )
+    }
 }
 
 export default SearchBar
+
+
